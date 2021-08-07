e4fdcde87c8a28a1b6153b93a53b49ffa73d097c
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-07 15:12:19 +0100

    Cart SVG

--- lib/widgets/cart.dart
+++ lib/widgets/cart.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:furniture_ui/constants/colors.dart';
 
 class Cart extends StatelessWidget {
   final int numberOfItemsInCart;
@@ -6,6 +8,30 @@ class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: 24,
+          height: 24,
+          child: SvgPicture.asset('assets/icons/cart.svg'),
+        ),
+        if (numberOfItemsInCart > 0)
+          Positioned(
+            top: -4,
+            right: -4,
+            child: Container(
+              width: 12,
+              height: 12,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 1),
+              ),
+            ),
+          ),
+      ],
+    );
   }
 }
