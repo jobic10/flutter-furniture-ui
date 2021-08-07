53f784edcdb0805f08b2408f80219d662614cfa6
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-07 18:51:34 +0100

    New color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color.fromRGBO(47, 74, 161, 1);
 const Color primaryPromoColor = Color(0XFF3150B4);
 const Color secondaryPromoColor = Color(0XFF0F2C91);
+const Color tagBackgroundColor = Color.fromRGBO(177, 173, 173, 0.25);
