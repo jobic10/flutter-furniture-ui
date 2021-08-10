d3984a38d8411413569a6757384f7708d4771797
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-10 15:17:31 +0100

    RenderFlex

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:furniture_ui/data/fake.dart';
 import 'package:furniture_ui/widgets/category_card.dart';
 import 'package:furniture_ui/widgets/header.dart';
+import 'package:furniture_ui/widgets/image_card.dart';
 import 'package:furniture_ui/widgets/promo_card.dart';
 import 'package:furniture_ui/widgets/section.dart';
 
@@ -17,34 +18,49 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Header(),
-            Section(
-              title: 'Categories',
-              children: Fake.categories
-                  .map(
-                    (e) => CategoryCard(
-                        title: e.title, iconPath: e.iconPath, onTap: () {}),
-                  )
-                  .toList(),
-            ),
-            Section(
-                title: 'Today\'s Promo',
-                children: Fake.promos
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Header(),
+              Section(
+                title: 'Categories',
+                children: Fake.categories
                     .map(
-                      (e) => PromoCard(
-                        backgroundImagePath: e.backgroundImagePath,
-                        reverseGradient: e.reverseGradient,
-                        subtitle: e.subtitle,
-                        title: e.title,
-                        caption: e.caption,
-                        imagePath: e.imagePath,
-                        tag: e.tag,
-                      ),
+                      (e) => CategoryCard(
+                          title: e.title, iconPath: e.iconPath, onTap: () {}),
                     )
-                    .toList())
-          ],
+                    .toList(),
+              ),
+              Section(
+                  title: 'Today\'s Promo',
+                  children: Fake.promos
+                      .map(
+                        (e) => PromoCard(
+                          backgroundImagePath: e.backgroundImagePath,
+                          reverseGradient: e.reverseGradient,
+                          subtitle: e.subtitle,
+                          title: e.title,
+                          caption: e.caption,
+                          imagePath: e.imagePath,
+                          tag: e.tag,
+                        ),
+                      )
+                      .toList()),
+              Section(
+                title: 'Trending Furniture',
+                children: Fake.trending
+                    .map((img) => ImageCard(imagePath: img))
+                    .toList(),
+              ),
+              Section(
+                title: 'Featured Furniture',
+                children: Fake.featured
+                    .map((img) => ImageCard(imagePath: img))
+                    .toList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/image_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ImageCard extends StatelessWidget {
+  final String imagePath;
+  const ImageCard({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 270,
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(7),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: Offset.zero,
+            blurRadius: 15,
+          )
+        ],
+      ),
+    );
+  }
+}
