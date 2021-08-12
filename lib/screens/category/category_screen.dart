28315aab7589554b4d776e39f0c00ab8b8c4b9e7
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-12 14:34:43 +0100

    Slivers

--- lib/screens/category/category_screen.dart
+++ lib/screens/category/category_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_ui/widgets/app_bottom_nav.dart';
+
+import 'widgets/header_sliver.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({Key? key}) : super(key: key);
@@ -10,6 +13,18 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: AppBottomNavigation(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: SliverPersistentHeader(
+            delegate: HeaderSliver(minExtent: 120, maxExtent: 120),
+            pinned: true,
+            floating: true,
+          ),
+        ),
+      ),
+    );
   }
 }
