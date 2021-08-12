cac42c621ad26b6ecb2ead9276de6358579c0f30
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-12 15:57:38 +0100

    Pad

--- lib/screens/category/widgets/header.dart
+++ lib/screens/category/widgets/header.dart
@@ -20,41 +20,47 @@ class Header extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 60,
-                alignment: Alignment.centerLeft,
-                child: InkWell(
-                  child: SvgPicture.asset('assets/icons/back.svg'),
-                  onTap: () {
-                    if (Navigator.of(context).canPop()) {
-                      Navigator.of(context).pop();
-                    }
-                  },
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 28,
+              vertical: 16,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 60,
+                  alignment: Alignment.centerLeft,
+                  child: InkWell(
+                    child: SvgPicture.asset('assets/icons/back.svg'),
+                    onTap: () {
+                      if (Navigator.of(context).canPop()) {
+                        Navigator.of(context).pop();
+                      }
+                    },
+                  ),
                 ),
-              ),
-              Text(
-                'Living Room',
-                style: TextStyle(fontSize: 18),
-              ),
-              Container(
-                width: 60,
-                alignment: Alignment.centerRight,
-                child: Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 10,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/icons/search.svg',
-                      height: 18,
-                    ),
-                    Cart(numberOfItemsInCart: Fake.numberOfItemsInCart)
-                  ],
+                Text(
+                  'Living Room',
+                  style: TextStyle(fontSize: 18),
                 ),
-              ),
-            ],
+                Container(
+                  width: 60,
+                  alignment: Alignment.centerRight,
+                  child: Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    spacing: 10,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/search.svg',
+                        height: 18,
+                      ),
+                      Cart(numberOfItemsInCart: Fake.numberOfItemsInCart)
+                    ],
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
